204331748c3c4140a7b2fc441b81c1375e13ec58
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-06-22 23:06:51 +0800

    feat: implement vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
@@ -8,16 +9,19 @@ import SalesTransactions from './pages/SalesTransactions';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="products" element={<Products />} />
-          <Route path="pos" element={<POS />} />
-          <Route path="sales" element={<SalesTransactions />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="products" element={<Products />} />
+            <Route path="pos" element={<POS />} />
+            <Route path="sales" element={<SalesTransactions />} />
+          </Route>
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 };
 
